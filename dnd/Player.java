8beb303a3622c5e1e00c20692654280e3cd9947c
JacksonPaull <41710038+JacksonPaull@users.noreply.github.com>
2020-04-08 18:00:49 -0500

    Init

---
+++ dnd/Player.java
@@ -0,0 +1,72 @@
+public class Player
+{
+    private String name;
+    private int roll;
+    private int hp;
+    private int hpCap;
+    private int number;
+
+    public String getName()
+    {
+      return name;
+    }
+    public void setName(String name)
+    {
+      this.name = name;
+    }
+
+    public int getRoll()
+    {
+      return roll;
+    } 
+    public void setRoll(int roll)
+    {
+      this.roll = roll;
+    }
+
+    public void setHp(int hp)
+    {
+      if(hp>hpCap)
+      {
+        setHpCap(hp);
+      }
+      this.hp = hp;
+    }
+    public int getHp()
+    {
+      return hp;
+    }
+
+    private void setHpCap(int hp)
+    {
+      hpCap = hp;
+    }
+    
+    public void setNumber(int num)
+    {
+      number = num;
+    }
+    public int getNumber()
+    {
+      return number;
+    }
+
+    public void heal(int newHp)
+    {
+      if(hp+newHp>hpCap)
+      {
+        hp = hpCap;
+      }
+      else
+        hp+=newHp;
+    }
+    public void damage(int hp)
+    {
+      if(this.hp-hp<0)
+      {
+        hp = 0;
+      }
+      else
+        this.hp-=hp;
+    }
+}
